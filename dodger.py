205884aed365b280895697646b962bd517724e24
Thoth19 <kpseipp@gmail.com>
2017-02-08 21:48:27 -0800

    Initial butchering in progress; not working yet

--- dodger.py
+++ dodger.py
@@ -1,6 +1,6 @@
 # One dimensional dodger atm
 import random
-
+HALF_BOARD_SIZE = 5 
 class Player(object):
     """docstring for Player"""
     def __init__(self, start):
@@ -17,7 +17,7 @@ class CleverPlayer(Player):
     """docstring for GeneticPlayer"""
     def update(self, dangers):
         if dangers[self.pos]:
-            if self.pos > 5:
+            if self.pos > HALF_BOARD_SIZE:
                 if dangers[self.pos - 1]:
                     self.pos += 1
                 else:
@@ -28,9 +28,9 @@ class CleverPlayer(Player):
                 else:
                     self.pos += 1
         else:
-            if self.pos > 5 and not dangers[self.pos-1]:
+            if self.pos > HALF_BOARD_SIZE and not dangers[self.pos-1]:
                 self.pos -= 1
-            elif self.pos < 5 and not dangers[self.pos+1]:
+            elif self.pos < HALF_BOARD_SIZE and not dangers[self.pos+1]:
                 self.pos += 1
         self.pos = max(0, min(9, self.pos))
         return self.pos
@@ -52,12 +52,52 @@ class LessCleverPlayer(Player):
         self.pos = max(0, min(9, self.pos))
         return self.pos
 
-        
+class LearningPlayer(Player):
+    # Note imcompat with this files gameplay
+    def update(self, action):
+        self.pos += action
+        return self.pos
+    def evaluation(self, session, graph_ops, saver):
+        pass
 
 class GeneticPlayer(Player):
     """docstring for GeneticPlayer"""
     def update(self, danger_ind):
         pass
+
+class Game(object):
+    """docstring for Game"""
+    def __init__(self, board_size, player):
+        super(Game, self).__init__()
+        self.board_size = board_size
+        self.player = player
+        self.reset()
+
+    def reset():
+        self.player.pos = HALF_BOARD_SIZE
+        self.score = 0
+        self.dangers = [0 for _ in range(board_size)]
+        self.alive = True
+
+    def step():
+        if not self.alive:
+            return -1
+        # player.update(self.dangers)       
+
+        # Move danger onto board and check for death
+        if DANGERS[player.pos] == 1:
+            self.alive = False
+            return self.score
+
+        # Reset dangers, and add a danger
+        self.dangers = [0 for _ in range(board_size)]
+        for i in range(score//10 + 1):
+            danger = random.randint(0,9)
+            self.dangers[danger] = 1
+        score += 1
+
+        return 0 # For no error
+
 def game(board_size, player):
     BOARD = list(range(board_size))
     DANGERS = [0 for _ in range(board_size)]
@@ -85,12 +125,13 @@ def game(board_size, player):
 
         score += 1
     return score
+if __name__ == '__main__':
+    
+    # Run single time
+    # bob = CleverPlayer(HALF_BOARD_SIZE)
+    # print(game(10, bob))
 
-# Run single time
-# bob = CleverPlayer(5)
-# print(game(10, bob))
-
-# Run the whole averaging thing
-bob = CleverPlayer(5)
-l = [game(10, bob) for _ in range(10000)]
-print(sum(l)/float(len(l)))
+    # Run the whole averaging thing
+    bob = CleverPlayer(HALF_BOARD_SIZE)
+    l = [game(10, bob) for _ in range(10000)]
+    print(sum(l)/float(len(l)))
